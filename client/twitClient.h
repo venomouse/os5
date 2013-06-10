10fcad4adaf8c6f739412d03da85509219a27ad7
yonatan <or.yonatan@gmail.com>
2013-06-10 18:18:28 +0300

    new file:   client/nbproject/configurations.xml 	new file:   client/nbproject/private/Default.properties 	new file:   client/nbproject/private/configurations.xml 	new file:   client/nbproject/private/private.xml 	new file:   client/nbproject/project.xml 	new file:   client/twitClient.h 	new file:   nbproject/Makefile-Debug.mk 	new file:   nbproject/Makefile-Release.mk 	new file:   nbproject/Makefile-impl.mk 	new file:   nbproject/Makefile-variables.mk 	new file:   nbproject/Package-Debug.bash 	new file:   nbproject/Package-Release.bash 	new file:   nbproject/configurations.xml 	new file:   nbproject/private/Makefile-variables.mk 	new file:   nbproject/private/configurations.xml 	new file:   nbproject/private/private.xml 	new file:   nbproject/project.xml 	new file:   server/Makefile~ 	new file:   server/nbproject/configurations.xml 	new file:   server/nbproject/private/configurations.xml 	new file:   server/nbproject/private/private.xml 	new file:   server/nbproject/project.xml 	modified:   server/twitServer.cpp 	deleted:    twitClient.cpp 	deleted:    twitServer.cpp

---
+++ client/twitClient.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   twitClient.h
+ * Author: yonatan
+ *
+ * Created on June 10, 2013, 5:26 PM
+ */
+
+#ifndef TWITCLIENT_H
+#define	TWITCLIENT_H
+
+int recvAll (int client_socket, char* buffer);
+
+
+
+#endif	/* TWITCLIENT_H */
+
--- server/twitServer.cpp
+++ server/twitServer.cpp
@@ -19,8 +19,9 @@ using namespace std;
 #define FAIL -1;
 #define MIN_PORT_NUM 1025
 #define MAX_PORT_NUM 65535
-#define BUFFER_SIZE 140
+#define BUFFER_SIZE 145
 #define NDEBUG 0
+#define LOCALHOST "127.0.0.1"
 
 int main(int argc, char *argv[]) {
 
@@ -49,12 +50,19 @@ int main(int argc, char *argv[]) {
 	//The port number is the command parameter
 	server_addr.sin_port = htons(atoi (argv [1]));
 	//TODO check error
-	inet_aton("127.0.0.1",&server_addr.sin_addr);
+	inet_aton(LOCALHOST,&server_addr.sin_addr);
 	memset(&(server_addr.sin_zero), '\0', 8);
 
+        if (setsockopt(server_socket,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int))==-1)
+        {
+            cerr << "Error: failed to setsockopt" << endl;
+        }
+    
+        
 	if (bind (server_socket, (struct sockaddr *) &server_addr, sizeof (server_addr)) < 0)
 	{
 		cerr << "Error: failed to bind the socket to the port" << endl;
+                exit(1);
 	}
 
 
@@ -63,6 +71,7 @@ int main(int argc, char *argv[]) {
 	if (listen (server_socket, SOMAXCONN) < 0)
 	{
 		cerr << "Error: failed to listen to the port" <<endl;
+                exit(1);
 	}
 
 	if (!NDEBUG)
@@ -73,9 +82,10 @@ int main(int argc, char *argv[]) {
 	while (1)
 	{
 		sockaddr_in client_addr;
-		char message_buffer[BUFFER_SIZE];
+		char message_buffer[BUFFER_SIZE] = "";
 		unsigned int client_length = sizeof (client_addr);
 		int client_socket;
+                
 
 		if ((client_socket = accept (server_socket, (struct sockaddr *)&client_addr, &client_length)) < 0)
 		{
@@ -84,19 +94,40 @@ int main(int argc, char *argv[]) {
 		}
 
 		int recv_result;
-		while ((recv_result = recv (client_socket, message_buffer, BUFFER_SIZE, 0)) > 0)
+                if (recvAll(client_socket,message_buffer,BUFFER_SIZE,0) != 0); 
+		else // (recvAll == 0)
 		{
-			//TODO it's more complicated than that
-			cout << message_buffer << endl;
+                        //disconnect (resolveClient(client_socket))
+                        cout << "Client disconnected" << endl;
 		}
-		if (recv_result == 0)
-		{
-			cout << "Client disconnected" << endl;
-		}
+                message_buffer[BUFFER_SIZE] = "";
 
 	}
 
 
 
 	return 0;
+}
+
+int recvAll (int client_socket, char* buffer)
+{
+    int recv_result;
+    char stringSize ;
+    char tempBuffer[BUFFER_SIZE] = "";
+    if ( ( (recv_result = recv (client_socket, stringSize, 1, 0)) <= 0)) 
+    {
+        return 0;
+    }
+    while ( ( (recv_result = recv (client_socket, tempBuffer, int(stringSize), 0)) > 0) && 
+                        (strlen(buffer)+strlen(tempBuffer) <= int(stringSize) ) ) 
+    {
+        strcat(buffer,tempBuffer);
+        tempBuffer  = "";
+    }
+    if (stringSize != strlen(buffer))
+    {
+        cerr << "Broken message" << endl;
+        return 0;
+    }
+    return stringSize;
 }
